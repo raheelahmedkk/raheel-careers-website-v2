dc22f5240cd039007314bb68d711e61e44666b68
raheelahmedkk <raheelfinancials@gmail.com>
2024-08-12 18:56:44 +0000

    Added html and a default banner

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from re import DEBUG
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, Raheel!</p>"
+    # return "<p>Hello, Raheel!</p>"
+  return render_template('home.html')  
 
 if __name__ == "__main__":
   # print("Im inside the main section")
