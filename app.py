5f250ff7fc2700e89acd0bab49e209049e27de38
raheelahmedkk <raheelfinancials@gmail.com>
2024-08-12 17:58:52 +0000

    message changed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Raheel!</p>"
 
 if __name__ == "__main__":
   # print("Im inside the main section")
