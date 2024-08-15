f30f21b44d686f9efc78ae00caa7f3509838e374
raheelahmedkk <raheelfinancials@gmail.com>
2024-08-15 16:36:53 +0000

    added styling using bootstrap

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
 from re import DEBUG
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 
-app = Flask(__name__)
+app = Flask(__name__) 
+
+JOBS = [
+    {
+      'id': 1,
+      'title': 'Data Analyst',
+      'location': 'Karachi, Pakistan',
+      'salary': 'Rs. 10,00,000'
+    },
+    {
+      'id': 2,
+      'title': 'Data Scientist',
+      'location': 'Karachi, Pakistan',
+      'salary': 'Rs. 20,00,000'
+    },
+    {
+      'id': 3,
+      'title': 'Web developer',
+      'location': 'Remote',
+      # 'salary': 'Rs. 8,00,000'
+  },
+  {
+      'id': 4,
+      'title': 'Database administrator',
+      'location': 'Karachi, Pakistan',
+      'salary': 'Rs. 12,00,000'
+  },
+  {
+      'id': 5,
+      'title': 'Frontend developer',
+      'location': 'Karachi, Pakistan',
+      'salary': 'Rs. 12,00,000'
+  }
+]
+
 
 @app.route("/")
 def hello_world():
     # return "<p>Hello, Raheel!</p>"
-  return render_template('home.html')  
+  return render_template('home.html', jobs=JOBS, company_name='RRR Technologies')  
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+
 
 if __name__ == "__main__":
   # print("Im inside the main section")
